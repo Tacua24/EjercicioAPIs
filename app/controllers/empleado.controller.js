27be455b4ad389945704a9820278b1088f4b423e
tacua <tiago24r@gmail.com>
2024-08-28 20:33:59 -0600

    commit 701

--- app/controllers/empleado.controller.js
+++ app/controllers/empleado.controller.js
@@ -1,112 +1,118 @@
-const db = require('../config/db.config.js');
-const Empleado = db.Empleado;
+const db = require("../config/db.config.js");
+const Empleado = db.empleados;
 
-// Create and Save a new Empleado
+// Crear y guardar un nuevo empleado
 exports.create = (req, res) => {
-  if (!req.body.PrimerNombre || !req.body.SegundoNombre || !req.body.PrimerApellido || !req.body.SegundoApellido || !req.body.Nit || !req.body.Salario || !req.body.Estatus || !req.body.id_departamento) {
-    return res.status(400).send({
-      message: "Todos los campos son obligatorios"
-    });
-  }
+    if (!req.body.PrimerNombre) {
+        res.status(400).send({
+            message: "El contenido no puede estar vacío."
+        });
+        return;
+    }
 
-  const Empleado = {
-    id_empleado: req.body.id_empleado,
-    primer_nombre: req.body.primer_nombre,
-    segundo_nombre: req.body.segundo_nombre,
-    primer_apellido: req.body.primer_apellido,
-    segundo_apellido: req.body.segundo_apellido,
-    nit: req.body.nit,
-    salario: req.body.salario,
-    estatus: req.body.estatus,
-    id_departamento: req.body.id_departamento
+    const empleado = {
+        id_empleado: req.body.id_empleado,
+        PrimerNombre: req.body.PrimerNombre,
+        SegundoNombre: req.body.SegundoNombre,
+        PrimerApellido: req.body.PrimerApellido,
+        SegundoApellido: req.body.SegundoApellido,
+        Nit: req.body.Nit,
+        Salario: req.body.Salario,
+        Estatus: req.body.Estatus,
+        id_departamento: req.body.id_departamento
+    };
+
+    Empleado.create(empleado)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Ocurrió un error al crear el Empleado."
+            });
+        });
 };
 
-  Empleado.create(empleado)
-   .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: err.message || "Ocurrió un error al crear el Empleado."
-      });
-    });
-};
-
-// Retrive all Empleados
+// Obtener todos los empleados
 exports.findAll = (req, res) => {
-  Empleado.findAll()
-   .then(data => {
-      res.send(data);
-    })
-   .catch(err => {
-      res.status(500).send({
-        message: err.message || "Ocurrió un error al recuperar los Empleados."
-      });
-    });
+    Empleado.findAll()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Ocurrió un error al obtener los empleados."
+            });
+        });
 };
 
-// Find a single Empleado by ID
+// Obtener un solo empleado por ID
 exports.findOne = (req, res) => {
     const id = req.params.id_empleado;
 
     Empleado.findByPk(id)
-   .then(data => {
-     if (data) {
-       res.send(data);
-     } else {
-       res.status(404).send({
-         message: `No se encontró el Empleado con id=${id}.`
-       });
-     }
-   })
-   .catch(err => {
-     res.status(500).send({
-       message: err.message || `Error al recuperar el Empleado con id=${id}.`
-     });
-   });
-}
+        .then(data => {
+            if (data) {
+                res.send(data);
+            } else {
+                res.status(404).send({
+                    message: `No se encontró un Empleado con id=${id}.`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `Error al obtener el Empleado con id=${id}.`
+            });
+        });
+};
 
-// Update a Empleado by the id in the request
+// Actualizar un empleado por ID
 exports.update = (req, res) => {
     const id = req.params.id_empleado;
 
     Empleado.update(req.body, {
-      where: { id_empleado: id }
+        where: { id_empleado: id }
     })
-   .then(num => {
-     if (num == 1) {
-       res.send({
-         message: "Empleado actualizado con éxito."
-       });
-     } else {
-       res.send({
-         message: `No se pudo actualizar el Empleado con id=${id}. Tal vez el Empleado no fue encontrado o el req.body está vacío.`
-       });
-     }
-   })
-}
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Empleado actualizado exitosamente."
+                });
+            } else {
+                res.send({
+                    message: `No se puede actualizar el Empleado con id=${id}.`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `Error al actualizar el Empleado con id=${id}.`
+            });
+        });
+};
 
-// Delete a Empleado by the id in the request
+// Eliminar un empleado por ID
 exports.delete = (req, res) => {
-  const id = req.params.id_empleado;
+    const id = req.params.id_empleado;
 
-  Empleado.destroy({
-    where: { id_empleado: id }
-  })
-  .then(num => {
-    if (num == 1) {
-      res.send({
-        message: "Empleado eliminado con éxito."
-      });
-    } else {
-      res.send({
-        message: `No se pudo eliminar el Empleado con id=${id}. Tal vez el Empleado no fue encontrado.`
-      });
-    }
-  })
-  .catch(err => {
-    res.status(500).send({
-      message: err.message || `Error al eliminar el Empleado con id=${id}.`
-    });
-  });
+    Empleado.destroy({
+        where: { id_empleado: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Empleado eliminado exitosamente."
+                });
+            } else {
+                res.send({
+                    message: `No se puede eliminar el Empleado con id=${id}.`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `Error al eliminar el Empleado con id=${id}.`
+            });
+        });
 };
